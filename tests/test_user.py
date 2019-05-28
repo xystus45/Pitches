c90b2a65c3a52a7cc344804827c3c457a4c2709b
xystus ngigi <xystusngigi@gmail.com>
2019-05-28 20:04:53 +0300

    add changes in test_user

--- tests/test_user.py
+++ tests/test_user.py
@@ -15,4 +15,4 @@ class UserModelTest(unittest.TestCase):
             self.new_user.password
 
     def test_password_verification(self):
-        self.assertTrue(self.new_user.verify_password('banana'))
+self.assertTrue(self.new_user.verify_password('banana'))
